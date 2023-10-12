f9268cd935b00684aa7e3def132ee7ce6444a265
Vrushali-Naikare <vrushalivilasnaikare@gmail.com>
2023-10-12 15:27:02 +0530

    files added

---
+++ react-login-form/src/components/MainHeader/MainHeader.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import Navigation from './Navigation';
+import classes from './MainHeader.module.css';
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes['main-header']}>
+      <h1>A Typical Login Page</h1>
+      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+    </header>
+  );
+};
+
+export default MainHeader;
